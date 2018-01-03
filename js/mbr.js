02354430893dd557ed064ace55aa3c8678a24050
Mark <mharrisondesign@mail.com>
2018-01-03 21:12:16 +0000

    Updated js, thanks page and additional image added to img folder

--- js/mbr.js
+++ js/mbr.js
@@ -1,4 +1,4 @@
-/*scroll to top functionality */
+//nav scroll to section
 function scrollto(id) {
     $('html, body').animate(
         {
@@ -7,29 +7,36 @@ function scrollto(id) {
     'slow');
 };
 
-function backTop(headerPic) {
+//scroll to top functionality
+
+function backTop(headerPic) { //scroll up to div headerPic slowly
     $('html, body').animate(
         {
-            scrollTop: $("#" + "topRight").offset().top
+            scrollTop:0
         },
         'slow');
 };
 
-/*show trails by country*/
-function showCountryEng() {
+//Form validation on submit
+function validate(){
+    onclick
+}
+
+//show trails by country tests
+/*function showCountryEng() {
     var eng = document.getElementById("ENG");
-    if (eng.style.display === 'none') {
-        eng.style.display = 'block';
+    if (eng.style.display === 'none') { //if it's not visible, 
+        eng.style.display = 'block'; //then make it visible
     } else {
-        eng.style.display = 'none';
+        eng.style.display = 'none'; //else don't display it
     }
 }
 function showCountrySco() {
     var sco = document.getElementById("SCO");
-    if (sco.style.display === 'none') {
+    if (sco.style.display === 'none') { 
         sco.style.display = 'block';
     } else {
-        sco.style.display = 'none';
+        sco.style.display = 'none'; 
     }
 }
 function showCountryWal() {
@@ -39,9 +46,68 @@ function showCountryWal() {
     } else {
         wal.style.display = 'none';
     }
-}
-/*NEW FUNCTION
-function showCountryEng() {
-    var show = document.getElementById("ENG");
-    var hide = document.getElementById("ENG");
 }*/
+
+//show trails by country - working code
+function showCountryEng() {
+    var eng = document.getElementById("ENG");
+    var sco = document.getElementById("SCO");
+    var wal = document.getElementById("WAL");
+    // If England trails are not visible
+    if (eng.style.display == 'none') {
+        // Make England trails visible
+        eng.style.display = 'block';
+        // Make Scotland and Wales invisible
+        sco.style.display = 'none';
+        wal.style.display = 'none';
+        // If England is visible
+    } else if (eng.style.display = 'block') {
+        // Keep it that way and hide the others
+        sco.style.display = 'none';
+        wal.style.display = 'none';
+    }
+}
+function showCountrySco() {
+    var eng = document.getElementById("ENG");
+    var sco = document.getElementById("SCO");
+    var wal = document.getElementById("WAL");
+    // If Scotland trails are not visible
+    if (sco.style.display == 'none') {
+        // Make Scotland trails visible
+        sco.style.display = 'block';
+        // Make Wales and England invisible
+        wal.style.display = 'none';
+        eng.style.display = 'none';
+        // If Scotland is visible
+    } else if (sco.style.display = 'block') {
+        // Keep it that way and hide the others
+        wal.style.display = 'none';
+        eng.style.display = 'none';
+    }
+}
+function showCountryWal() {
+    var eng = document.getElementById("ENG");
+    var sco = document.getElementById("SCO");
+    var wal = document.getElementById("WAL");
+    // If Wales trails are not visible
+    if (wal.style.display == 'none') {
+        // Make Wales trails visible
+        wal.style.display = 'block';
+        // Make Scotland and England invisible
+        sco.style.display = 'none';
+        eng.style.display = 'none';
+        // If Wales is visible
+    } else if (wal.style.display = 'block') {
+        // Keep it that way and hide the others
+        sco.style.display = 'none';
+        eng.style.display = 'none';
+    }
+}
+function showAll() {
+    var eng = document.getElementById("ENG");
+    var sco = document.getElementById("SCO");
+    var wal = document.getElementById("WAL");
+    eng.style.display = "block";
+    sco.style.display = "block";
+    wal.style.display = "block";
+}
