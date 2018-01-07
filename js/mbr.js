d1c9490c3276ba6acf346c5c3c031da63b6429ab
Mark <mharrisondesign@mail.com>
2018-01-07 21:09:28 +0000

    Addition of js validation to form, new imagery and videos, updated css

--- js/mbr.js
+++ js/mbr.js
@@ -18,9 +18,61 @@ function backTop(headerPic) { //scroll up to div headerPic slowly
 };
 
 //Form validation on submit
-function validate(){
-    onclick
+/*function formCheck(){
+    var val = document.forms["Form"]["firstName"], "secondName", "addone", "addtwo", "town", "pcode", "email"].value;
+    if (val ==""){
+        alert("Please fill in your first name");
+        return false;
+    }
+}*/
+
+function formCheck() {
+    var val = document.getElementById("fillMeIn").value;
+    var val = document.getElementById("fillMeIn2").value;
+    var val = document.getElementById("fillMeIn3").value;
+    var val = document.getElementById("fillMeIn4").value;
+    var val = document.getElementById("fillMeIn5").value;
+    var val = document.getElementById("fillMeIn6").value;
+    var val = document.getElementById("fillMeIn7").value;
+    if (val == "") {
+        alert("Please fill in the missing fields");
+        document.getElementById("fillMeIn").style.borderColor = "red";
+        document.getElementById("fillMeIn2").style.borderColor = "red";
+        document.getElementById("fillMeIn3").style.borderColor = "red";
+        document.getElementById("fillMeIn4").style.borderColor = "red";
+        document.getElementById("fillMeIn5").style.borderColor = "red";
+        document.getElementById("fillMeIn6").style.borderColor = "red";
+        document.getElementById("fillMeIn7").style.borderColor = "red";
+        return false;
+    }
+    else {
+        document.getElementById("fillMeIn").style.borderColor = "green";
+        document.getElementById("fillMeIn2").style.borderColor = "green";
+        document.getElementById("fillMeIn3").style.borderColor = "green";
+        document.getElementById("fillMeIn4").style.borderColor = "green";
+        document.getElementById("fillMeIn5").style.borderColor = "green";
+        document.getElementById("fillMeIn6").style.borderColor = "green";
+        document.getElementById("fillMeIn7").style.borderColor = "green";
+    }
 }
+
+/*
+var form = document.forms["mbrForm"]
+var val = ["fillMeIn", "fillMeIn2", "fillMeIn3", "fillMeIn4", "fillMeIn5", "fillMeIn6", "fillMeIn7"];
+
+function formCheck() {
+    if(form['fillMeIn].value{
+        alert("Please fill this field in");
+        elem.fillMeIn.focus();
+        error.className = "error";
+        return false;
+    }
+    else {
+        error.className = "noerror";
+        return true;
+    }
+}
+form.onsubmit = formCheck;*/
 
 //show trails by country tests
 /*function showCountryEng() {
