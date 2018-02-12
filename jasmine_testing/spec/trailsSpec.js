8732d8f42ccf205091598f39a61f90ba81d74b30
Mark <mharrisondesign@mail.com>
2018-02-12 21:55:51 +0000

    Remove duplicate directory

--- jasmine_testing/spec/trailsSpec.js
+++
@@ -1,7 +0,0 @@
-describe("trails", function(){
-    describe("showCountryEng function", function() {
-        it("should show and hide the div on click", function() {
-            expect(showCountryEng()).toBe(true);
-        });
-    });
-});
